a8c6fb5babf3be08a5e907f4ab036f3164b5c321
tanawatgravitechdev <tanawatgravitechdev@gmail.com>
2024-07-11 15:43:12 +0700

    add function convert multiple file 👁‍🗨

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import xlsxwriter
+import os
 
-workbook = xlsxwriter.Workbook('demo.xlsx')
-worksheet = workbook.add_worksheet()
+path = './txt_dir'
+dir_list = os.listdir(path)
 
 
-f = open('./BOM.txt', 'r+')
-listTmp = f.read().split('\n')
-key = 0
-while key < len(listTmp):
-    item = listTmp[key]
-    if(item.replace(' ','') != ""):
-        lt = [v for v in item.split('  ') if v != '']
-        i = 0
-        for x in lt:
-            worksheet.write(key+1, i, x)
-            i += 1
-        print(lt)
-    key = key + 1
+for i in dir_list:
+    workbook = xlsxwriter.Workbook("./output/"+i.split('.')[0]+'.xlsx')
+    worksheet = workbook.add_worksheet()
 
-workbook.close()
+    print('./txt_dir/'+i)
+    with open('./txt_dir/'+i, 'r', encoding='utf-8') as f:
+        listTmp = f.read().split('\n')
+        key = 0
+        while key < len(listTmp):
+            item = listTmp[key].replace('	%','%').replace(' %','%')
+            if(item.replace(' ','') != ""):
+                lt = [v for v in item.split() if v != '']
+                i = 0
+                for x in lt:
+                    worksheet.write(key+1, i, x)
+                    i += 1
+                print(lt)
+            key = key + 1
+
+        workbook.close()
